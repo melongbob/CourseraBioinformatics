f0b208e25e415f6f75859bcf5b5e272bd449018a
Yun Kim <melongbob@gmail.com>
2018-02-13 22:47:00 -0800

    Beginning of Coursera Bioinformatics

---
+++ replication.py
@@ -0,0 +1,44 @@
+#Output a list of most frequent k-mers in Text
+def FrequentWords(Text, k):
+	FrequentPatterns = []
+	Count = CountDict(Text, k)
+	m = max(Count.values())
+	for i in Count:
+		if Count[i] == m:
+			FrequentPatterns.append(Text[i:i+k])
+	FrequentPatternsNoDuplicates = remove_duplicates(FrequentPatterns)
+	return FrequentPatternsNoDuplicates
+
+#Output a list of objects from Items without duplicates
+def remove_duplicates(Items):
+	ItemsNoDuplicates = []
+	for i in range(len(Items)):
+		if Items[i] not in ItemsNoDuplicates:
+			ItemsNoDuplicates.append(Items[i])
+	return ItemsNoDuplicates
+
+#Count k-mers in Text
+def CountDict(Text, k):
+    Count = {} 
+    for i in range(len(Text)-k+1):
+        Pattern = Text[i:i+k]
+        Count[i] = PatternCount(Pattern, Text)
+    return Count
+
+#Counts number of 'Patterns' in the 'Text'
+def PatternCount(Pattern, Text):
+    count = 0
+    for i in range(len(Text)-len(Pattern)+1):
+        if Text[i:i+len(Pattern)] == Pattern:
+            count = count + 1
+    return count
+
+#Given 'Pattern' and the 'Text'
+Text = "ATCAATGATCAACGTAAGCTTCTAAGCATGATCAAGGTGCTCACACAGTTTATCCACAACCTGAGTGGATGACATCAAGATAGGTCGTTGTATCTCCTTCCTCTCGTACTCTCATGACCACGGAAAGATGATCAAGAGAGGATGATTTCTTGGCCATATCGCAATGAATACTTGTGACTTGTGCTTCCAATTGACATCTTCAGCGCCATATTGCGCTGGCCAAGGTGACGGAGCGGGATTACGAAAGCATGATCATGGCTGTTGTTCTGTTTATCTTGTTTTGACTGAGACTTGTTAGGATAGACGGTTTTTCATCACTGACTAGCCAAAGCCTTACTCTGCCTGACATCGACCGTAAATTGATAATGAATTTACATGCTTCCGCGACGATTTACCTCTTGATCATCGATCCGATTGAAGATCTTCAATTGTTAATTCTCTTGCCTCGACTCATAGCCATGATGAGCTCTTGATCATGTTTCCTTAACCCTCTATTTTTTACGGAAGAATGATCAAGCTGCTGCTCTTGATCATCGTTTC"
+
+Pattern = "TGATCA"
+
+k = 10
+
+#Print the result
+print(FrequentWords(Text, k))
