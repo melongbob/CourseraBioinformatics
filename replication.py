ec56dcee691c9889c3a075744080efebbf9b234d
Yun Kim <melongbob@gmail.com>
2018-02-15 19:35:27 -0800

    updated SymbolArray function to make it more efficient

--- replication.py
+++ replication.py
@@ -5,8 +5,15 @@ def SymbolArray(Genome, symbol):
         array = {}
         n = len(Genome)
         ExtendedGenome = Genome + Genome[0:n//2]
-        for i in range(n):
-                array[i] = PatternCount(symbol, ExtendedGenome[i:i+(n//2)])
+
+        array[0] = PatternCount(symbol, Genome[0:n//2])
+        
+        for i in range(1, n):
+                array[i] = array[i-1]
+                if ExtendedGenome[i-1] == symbol:
+                        array[i] = array[i] - 1
+                if ExtendedGenome[i+(n//2)-1] == symbol:
+                        array[i] + 1
         return array
 
 #Find all positions where the pattern occurs in the genome
