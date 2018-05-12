3ebd8019d60d70fba4f001fdba977a74b427d682
Yun Kim <melongbob@gmail.com>
2018-05-12 11:34:12 -0700

    added Countwithpseudocouts function

--- motifs.py
+++ motifs.py
@@ -1,3 +1,28 @@
+#------------------------- Week 4 ----------------------------------
+
+# 
+# Input: list Motifs
+# Output: dictionary count (count with pseudo-counts)
+
+def CountWithPseudocounts(Motifs):
+    t = len(Motifs)
+    k = len(Motifs[0])
+    # insert your code here
+
+    count = {}
+    for symbol in "ACGT":
+        count[symbol] = []
+        for j in range(k):
+            count[symbol].append(1)
+
+    for i in range(t):
+        for j in range(k):
+            symbol = Motifs[i][j]
+            count[symbol][j] += 1
+
+    return count
+
+
 #------------------------- Week 3 ----------------------------------
 
 #Finds best Motifs
